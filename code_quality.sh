99d38f7afcb3076451dee106c7ec256074381e8f
Iv Sed <d.zhaksybayev@kmic.kz>
2025-06-11 12:18:25 +0500

    code_quality.sh upgrade

--- code_quality.sh
+++ code_quality.sh
@@ -2,21 +2,36 @@
 
 set -e
 
-# Run flake8, pylint, mypy and capture their exit statuses
+overall_status=0
+
 flake8 ./
 flake8_status=$?
+if [[ $flake8_status -ne 0 ]]; then
+  overall_status=1
+fi
 
 pylint ./
 pylint_status=$?
+if [[ $pylint_status -ne 0 ]]; then
+  overall_status=1
+fi
 
 mypy app/
 mypy_status=$?
+if [[ $mypy_status -ne 0 ]]; then
+  overall_status=1
+fi
 
 bandit -r app/
 bandit_status=$?
+if [[ $bandit_status -ne 0 ]]; then
+  overall_status=1
+fi
 
-# Fail the job if any tool failed
-if [[ $flake8_status -ne 0 || $pylint_status -ne 0 || $mypy_status -ne 0 || $bandit_status -ne 0 ]]; then
+if [[ $overall_status -ne 0 ]]; then
   echo "Code quality checks failed."
   exit 1
+else
+  echo "All code quality checks passed!"
+  exit 0
 fi
